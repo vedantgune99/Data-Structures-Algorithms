4cecd874c862cdbe4d228bbf1c9f32d09a5d9087
Vedant Gune <vedantgune41@gmail.com>
2023-02-28 18:17:37 +0530

    linear search algo on array

---
+++ Algorithms/Searching Algos/linear_search.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX 5
+
+// Function Prototype --->
+int linearSearch(int arr[], int data, int len);
+
+int main()
+{
+    int arr[MAX];
+    int i = 0, x;
+    while (i++ < MAX)
+    {
+        printf("\n>>>Enter element %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\n>>>Enter data you want to find : ");
+    scanf("%d", &x);
+
+    int index = linearSearch(arr, x, MAX);
+
+    if (index >= 0)
+        printf("\nElement found at %d!", index);
+    else
+        printf("\nElement not found!");
+
+    return 0;
+}
+
+// Linear Search on Array --->
+int linearSearch(int arr[], int data, int len)
+{
+    int index = 0;
+    while (index++ < len)
+    {
+        if (arr[index] == data)
+            return index;
+    }
+    return (-1);
+}
