c80ff676174ccce7ee67dee8432ba096e680c00b
Vedant Gune <vedantgune41@gmail.com>
2023-03-10 23:23:19 +0530

    basic sorting with selection sort

--- Algorithms/Sorting Algos/selection_sort.c
+++ Algorithms/Sorting Algos/selection_sort.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
+#define MAX 5
 
 void selection_sort(int arr[], int len);
+int find_min_index(int arr[], int start, int end);
 
-int find_min(int arr[], int start, int end)
+int main()
+{
+    int num[MAX] = {5, 4, 3, 2, 1};
+    // int min = find_min(num, 1, 10);
+
+    printf("\nBefore Sorting....");
+    for (int i = 0; i < MAX; i++)
+        printf("%d, ", num[i]);
+
+    selection_sort(num, MAX);
+    printf("\nAfter Sorting....");
+    for (int i = 0; i < MAX; i++)
+        printf("%d, ", num[i]);
+
+    return 0;
+}
+
+int find_min_index(int arr[], int start, int end)
 {
     int min = arr[start];
-    for (int i = start; i <= end; i++)
+    for (int i = start; i < end; i++)
     {
         if (min >= arr[i])
             min = arr[i];
     }
-    return min;
-}
-
-int main()
-{
-    int num[] = {0, 2, 10, 3, 20, 7, 41, 9, 75, 8, 5};
-    int min = find_min(num, 1, 10);
-    printf("Minimum : %d", min);
-    return 0;
+    for (int j = start; j < end; j++)
+    {
+        if (min == arr[j])
+            return j;
+    }
 }
 
 void selection_sort(int arr[], int len)
 {
+    for (int i = 0; i < len; i++)
+    {
+        int min = find_min_index(arr, i, len);
+        if (arr[i] >= arr[min])
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
 }
